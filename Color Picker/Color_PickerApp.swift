4a495b834b6d53accb36802af8fc62283b2f4ca9
Jake Smolowe <jakesmolowe@gmail.com>
2023-12-08 13:45:40 -0800

    Initial Commit

---
+++ Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by Jake Smolowe on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color Picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Color Picker
+//
+//  Created by Jake Smolowe on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
